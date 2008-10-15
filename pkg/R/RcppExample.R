2b704326d8770e2fe1b4630b0f32397d32aeb1ec
edd (Dirk Eddelbuettel) <edd@debian.org>
2008-10-15 02:27:17 +0000

    changed .Call to load from Rcpp reindented throughout to ESS / R Core standard
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@17 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/RcppExample.R
+++ pkg/R/RcppExample.R
@@ -1,55 +1,56 @@
 RcppExample <- function(params, nlist, numvec, nummat, df, datevec, stringvec,
                         fnvec, fnlist) {
 
-# Most of the input parameter checking here is not really
-# necessary because it is done in the Rcpp code.
-  
-  # Check that params is properly formatted.
-  if(!is.list(params) || length(params) == 0) {
-    stop("The params parameter must be a non-empty list");
-  }
+    ## Most of the input parameter checking here is not really
+    ## necessary because it is done in the Rcpp code.
 
-  # Check nlist
-  if(!is.list(nlist) || length(nlist) == 0) {
-    stop("The nlist parameter must be a non-empty list");
-  }
-  if(length(nlist) != length(names(nlist))) {
-    stop("The values in nlist must be named")
-  }
-  if(!is.numeric(unlist(nlist))) {
-    stop("The values in nlist must be numeric")
-  }
+    ## Check that params is properly formatted.
+    if(!is.list(params) || length(params) == 0) {
+        stop("The params parameter must be a non-empty list");
+    }
 
-  # Check numvec argument
-  if(!is.vector(numvec)) {
-    stop("numvec must be a vector");
-  }
+    ## Check nlist
+    if(!is.list(nlist) || length(nlist) == 0) {
+        stop("The nlist parameter must be a non-empty list");
+    }
+    if(length(nlist) != length(names(nlist))) {
+        stop("The values in nlist must be named")
+    }
+    if(!is.numeric(unlist(nlist))) {
+        stop("The values in nlist must be numeric")
+    }
 
-  # Check nummat argument
-  if(!is.matrix(nummat)) {
-    stop("nummat must be a matrix");
-  }
-  
-  # Finally ready to make the call...
-  val <- .Call("Rcpp_Example", params, nlist, numvec, nummat,
-               df, datevec, stringvec, fnvec, fnlist,
-               PACKAGE="RcppTemplate")
+    ## Check numvec argument
+    if(!is.vector(numvec)) {
+        stop("numvec must be a vector");
+    }
 
-  # Define a class for the return value so we can control what gets
-  # printed when a variable assigned this value is typed on a line by itself.
-  # This has the effect of calling the function print.RcppExample(). The
-  # function (defined below) simply prints the names of the fields that are
-  # available. Access each field with val$name.
-  class(val) <- "RcppExample"
-  
-  val
+    ## Check nummat argument
+    if(!is.matrix(nummat)) {
+        stop("nummat must be a matrix");
+    }
+
+    ## Finally ready to make the call...
+    val <- .Call("Rcpp_Example", params, nlist, numvec, nummat,
+                 df, datevec, stringvec, fnvec, fnlist,
+                 PACKAGE="Rcpp"
+                 )
+
+    ## Define a class for the return value so we can control what gets
+    ## printed when a variable assigned this value is typed on a line by itself.
+    ## This has the effect of calling the function print.RcppExample(). The
+    ## function (defined below) simply prints the names of the fields that are
+    ## available. Access each field with val$name.
+    class(val) <- "RcppExample"
+
+    val
 }
 
 print.RcppExample <- function(x,...) {
-  cat('Names defined in RcppExample return list:\n')
-  cat('(Use result$name to access)\n')
-  namevec <- names(x)
-  for(i in 1:length(namevec))
-    cat(i, ': ', namevec[i], '\n')
+    cat('Names defined in RcppExample return list:\n')
+    cat('(Use result$name to access)\n')
+    namevec <- names(x)
+    for(i in 1:length(namevec))
+        cat(i, ': ', namevec[i], '\n')
 }
 
