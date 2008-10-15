337033a2433bb30bc4db9bb86ded4d6299cbe7e5
edd <edd@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-10-15 02:28:04 +0000

    changed from RcppTemplate to Rcpp reindented throughout to ESS / R Core standard
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@18 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/RcppTemplateVersion.R
+++ pkg/R/RcppTemplateVersion.R
@@ -1,11 +1,12 @@
-RcppTemplateVersion <- function() {
+RcppVersion <- function() {
 
-  version <- installed.packages()["RcppTemplate","Version"]
-                                 
-  # Print version of this package
-  cat('\nRcppTemplate package Version ', version, '\n\n')
+    version <- installed.packages()["Rcpp","Version"]
 
-  # Print version of Rcpp/RcppTemplate used to build this package
-  licenseFile <- file(system.file(".","LICENSE-Rcpp.txt",package="RcppTemplate"),"r")
-  writeLines(readLines(licenseFile)[1:4])
+    ## Print version of this package
+    cat('\nRcpp package Version ', version, '\n\n')
+
+    ## Print version of Rcpp/RcppTemplate used to build this package
+    licenseFile <- file(system.file(".", "LICENSE-Rcpp.txt", package="Rcpp"),"r")
+    writeLines(readLines(licenseFile)[1:4])
+    invisible(NULL)
 }
