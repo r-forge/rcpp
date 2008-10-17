f045147446ff4599fe7b818d5842fbbc38742858
edd <edd@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-10-17 02:38:18 +0000

    report 5 lives from License file remove RcppDemo() from zzz.R as vignette("RcppAPI") works better anyway for the sole working / useful part of the demo
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@40 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/zzz.R
+++ pkg/R/zzz.R
@@ -3,32 +3,4 @@
     library.dynam("Rcpp", pkg, lib)
 }
 
-# Runs Rcpp demos interactively.
-RcppDemo <- function() {
-    demoCode <- demo(package="Rcpp")$results[,3]
-    if(length(demoCode) == 0) {
-        return('No demos found in package RcppTemplate')
-    }
-    while(TRUE) {
-        for(i in seq(1,length(demoCode))) {
-            cat(i, ': ', demoCode[i], '\n')
-        }
-        suppressWarnings(demoNum <- as.integer(readline(prompt="Enter demo number to run [0 to view descriptions, RETURN to exit]: ")))
-
-        if(is.na(demoNum)) {
-            cat("Didn't get a number, exiting.\n")
-            break
-        }
-        else if(demoNum == 0) {
-            print(demo(package="Rcpp"))
-        }
-        else {
-            if(demoNum < 0 || demoNum > length(demoCode)) {
-                cat('Number out of range, try again...\n')
-                next
-            }
-            demo(demoCode[demoNum], character.only=TRUE)
-        }
-    }
-}
 
