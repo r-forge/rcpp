2bbaa13aa7b33f65b9341fdc6e691ca9cf390847
edd (Dirk Eddelbuettel) <edd@debian.org>
2008-10-17 02:34:45 +0000

    removed demo/ dir: the ReadRcppAPI function is done better by vignette() in base R, and the sine examples are useless in the context
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@38 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/demo/SincSurface.R
+++
@@ -1,9 +0,0 @@
-# Plots z = 10 sin(r)/r
-x <- seq(-10, 10, length= 30)
-y <- x
-f <- function(x,y) { r <- sqrt(x^2+y^2); 10 * sin(r)/r }
-z <- outer(x, y, f)
-z[is.na(z)] <- 1
-op <- par(bg = "white")
-persp(x, y, z, theta = 30, phi = 30, expand = 0.5, col = "lightblue",
-      main="z = 10 sin(r)/r")
