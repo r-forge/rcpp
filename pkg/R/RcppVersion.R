27fad6e1911820a79df8fa015ddd6b55b478384d
edd (Dirk Eddelbuettel) <edd@debian.org>
2008-10-17 02:38:18 +0000

    report 5 lives from License file remove RcppDemo() from zzz.R as vignette("RcppAPI") works better anyway for the sole working / useful part of the demo
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@40 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/RcppVersion.R
+++ pkg/R/RcppVersion.R
@@ -7,6 +7,6 @@ RcppVersion <- function() {
 
     ## Print version of Rcpp/RcppTemplate used to build this package
     licenseFile <- file(system.file(".", "LICENSE-Rcpp.txt", package="Rcpp"),"r")
-    writeLines(readLines(licenseFile)[1:4])
+    writeLines(readLines(licenseFile)[1:5])
     invisible(NULL)
 }
