4fa0959b25c9467466f84439213924b9d09d0dee
edd (Dirk Eddelbuettel) <edd@debian.org>
2008-10-21 02:20:34 +0000

    delete old windows helper file
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@49 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inst/doc/MakeWinBin.bat
+++
@@ -1,6 +0,0 @@
-rem Check directory paths for accuracy
-rem
-set PATH=C:\Rtools\bin;C:\Program Files\R\R-2.3.1\bin;C:\Program Files\HTML Help Workshop;C:\Dev-Cpp\bin;%PATH%
-set MINGW_ROOT=C:\Dev-Cpp
-set RHOME=C:\Program Files\R\R-2.3.1
-Rcmd build --no-vignettes --force --binary --use-zip %1
