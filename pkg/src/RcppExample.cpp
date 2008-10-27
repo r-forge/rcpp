267a579ff3db58c83d76fe1c5aa6224234a37923
edd <edd@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-10-27 04:15:17 +0000

    new example RcppParamsExample in R / C++
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@56 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/RcppExample.cpp
+++ pkg/src/RcppExample.cpp
@@ -326,3 +326,53 @@ RcppExport SEXP Rcpp_Example(SEXP params, SEXP nlist,
 
     return rl;
 }
+
+
+RcppExport SEXP RcppParamsExample(SEXP params) {
+
+    SEXP  rl=R_NilValue; // Use this when there is nothing to be returned.
+    char* exceptionMesg=NULL;
+
+    try {
+
+	// Get parameters in params.
+	RcppParams rparam(params);
+	string method      = rparam.getStringValue("method");
+	double tolerance   = rparam.getDoubleValue("tolerance");
+	int    maxIter     = rparam.getIntValue("maxIter");
+	RcppDate startDate = rparam.getDateValue("startDate");
+	
+	Rprintf("\nIn C++, seeing the following value\n");
+	Rprintf("Method argument    : %s\n", method.c_str());
+	Rprintf("Tolerance argument : %f\n", tolerance);
+	Rprintf("MaxIter argument   : %d\n", maxIter);
+	Rprintf("Start date argument: %04d-%02d-%02d\n", 
+		startDate.getYear(), startDate.getMonth(), startDate.getDay());
+
+	// Build result set to be returned as a list to R.
+	RcppResultSet rs;
+
+	rs.add("method",    method);
+	rs.add("tolerance", tolerance);
+	rs.add("maxIter",   maxIter);
+	rs.add("startDate", startDate);
+
+	// Instead of returning selected input parameters as we did in
+	// the last statements, the entire input parameter list can be
+	// returned like this:
+	rs.add("params", params, false);  // 'false' to not PROTECT params
+
+	// Get the list to be returned to R.
+	rl = rs.getReturnList();
+	
+    } catch(std::exception& ex) {
+	exceptionMesg = copyMessageToR(ex.what());
+    } catch(...) {
+	exceptionMesg = copyMessageToR("unknown reason");
+    }
+    
+    if(exceptionMesg != NULL)
+	error(exceptionMesg);
+
+    return rl;
+}
