bcff367655ee3baac12eb62e989edc094585db9f
edd (Dirk Eddelbuettel) <edd@debian.org>
2008-11-01 15:04:11 +0000

    small RcppVersion cleanup.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@65 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/RcppVersion.R
+++ pkg/R/RcppVersion.R
@@ -3,10 +3,12 @@ RcppVersion <- function() {
     version <- installed.packages()["Rcpp","Version"]
 
     ## Print version of this package
-    cat('\nRcpp package Version ', version, '\n\n')
+    cat('\nRcpp package Version ', version, '\n')
 
     ## Print version of Rcpp/RcppTemplate used to build this package
-    licenseFile <- file(system.file(".", "LICENSE-Rcpp.txt", package="Rcpp"),"r")
-    writeLines(readLines(licenseFile)[1:5])
+    licenseFile <- system.file(".", "LICENSE-Rcpp.txt", package="Rcpp")
+    con <- file(licenseFile, "r")
+    writeLines(readLines(con)[1:4])
+    close(con)
     invisible(NULL)
 }
