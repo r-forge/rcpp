40b4f127254f69ffe6cdb930bc2c5e324b388354
edd (Dirk Eddelbuettel) <edd@debian.org>
2008-11-06 02:40:02 +0000

    added RcppDateExample
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@71 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/RcppExample.R
+++ pkg/R/RcppExample.R
@@ -123,3 +123,31 @@ RcppParamsExample <- function(params) {
 
     val
 }
+
+RcppDateExample <- function(dv, dtv) {
+
+    ## Check that params is properly set.
+    if (missing(dv)) {
+        cat("\nIn R, setting default argument for dv\n")
+        dv <- Sys.Date() + -2:2
+    }
+
+    if (missing(dtv)) {
+        cat("\nIn R, setting default argument for dtv\n")
+        dtv <- Sys.time() + (-2:2)*0.5
+    }
+
+    ## Make the call...
+    val <- .Call("RcppDateExample",
+                 dv, dtv,
+                 PACKAGE="Rcpp")
+
+    ## Define a class for the return value so we can control what gets
+    ## printed when a variable assigned this value is typed on a line by itself.
+    ## This has the effect of calling the function print.RcppExample(). The
+    ## function (defined below) simply prints the names of the fields that are
+    ## available. Access each field with val$name.
+    class(val) <- "RcppExample"
+
+    val
+}
