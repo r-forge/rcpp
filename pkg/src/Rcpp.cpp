cb2c730338a70be437a7e1f4aaa6a3de8ef76653
edd <edd@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-11-28 21:25:18 +0000

    small typo correction in header
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@92 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/Rcpp.cpp
+++ pkg/src/Rcpp.cpp
@@ -1,4 +1,4 @@
-// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; ; tab-width: 8 -*-
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; tab-width: 8 -*-
 //
 // Rcpp.cpp: Part of the R/C++ interface class library, Version 5.0
 //
