442884bb32b66e7e18bf89d4ef2c0527426bd56d
edd (Dirk Eddelbuettel) <edd@debian.org>
2009-12-20 14:51:44 +0000

    minor simplification to GSL-using example employing new RcppSexp type
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@206 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inst/examples/RcppInline/RcppInlineWithLibsExamples.r
+++ pkg/inst/examples/RcppInline/RcppInlineWithLibsExamples.r
@@ -1,4 +1,4 @@
-#!/usr/bin/r
+#!/usr/bin/r -t
 
 suppressMessages(library(Rcpp))
 
@@ -14,7 +14,7 @@ firstExample <- function() {
     printf("generator type: %s\\n", gsl_rng_name (r));
     printf("seed = %lu\\n", gsl_rng_default_seed);
     v = gsl_rng_get (r);
-    printf("first value = %f\\n", v);
+    printf("first value = %.0f\\n", v);
     return R_NilValue;
     '
 
@@ -35,8 +35,7 @@ secondExample <- function() {
 
     ## now use Rcpp to pass down a parameter for the seed
     gslrng <- '
-    RcppVector<int> vec(par);
-    int seed = vec(0);
+    int seed = RcppSexp(par).asInt();
 
     gsl_rng *r;
     gsl_rng_env_setup();
@@ -50,14 +49,10 @@ secondExample <- function() {
     #ifndef BeSilent
     printf("generator type: %s\\n", gsl_rng_name (r));
     printf("seed = %d\\n", seed);
-    printf("first value = %f\\n", v);
+    printf("first value = %.0f\\n", v);
     #endif
 
-    RcppResultSet rs;           // Build result set to be returned as a list to R.
-    rs.add("value", v);         // vec as named element with name "vec"
-    SEXP rl = rs.getReturnList();    // Get the list to be returned to R.
-
-    return rl;
+    return RcppSexp(v).asSexp();
     '
 
     ## turn into a function that R can call
@@ -69,7 +64,7 @@ secondExample <- function() {
                       cppargs="-I/usr/include",
                       libargs="-lgsl -lgslcblas")
     cat("\n\nCalling second example without -DBeSilent set\n")
-    print(funx(0)["value"])
+    print(funx(0))
 
     funx <- cfunction(signature(par="numeric"), gslrng,
                       includes="#include <gsl/gsl_rng.h>",
@@ -77,58 +72,7 @@ secondExample <- function() {
                       cppargs="-I/usr/include -DBeSilent",
                       libargs="-lgsl -lgslcblas")
     cat("\n\nCalling second example with -DBeSilent set\n")
-    print(funx(1)["value"])
-
-    invisible(NULL)
-}
-
-thirdExample <- function() {
-
-    ## now use Rcpp to pass down a parameter for the seed
-    gslrng <- '
-    RcppVector<int> vec(par);
-    int seed = vec(0);
-
-    gsl_rng *r;
-    gsl_rng_env_setup();
-    double v;
-
-    r = gsl_rng_alloc (gsl_rng_default);
-
-    gsl_rng_set (r, (unsigned long) seed);
-    v = gsl_rng_get (r);
-
-    #ifndef BeSilent
-    printf("generator type: %s\\n", gsl_rng_name (r));
-    printf("seed = %d\\n", seed);
-    printf("first value = %f\\n", v);
-    #endif
-
-    RcppResultSet rs;           // Build result set to be returned as a list to R.
-    rs.add("value", v);         // vec as named element with name "vec"
-    SEXP rl = rs.getReturnList();    // Get the list to be returned to R.
-
-    return rl;
-    '
-
-    ## turn into a function that R can call
-    ## compileargs redundant on Debian/Ubuntu as gsl headers are found anyway
-    ## use additional define for compile to suppress output
-    funx <- cfunction(signature(par="numeric"), gslrng,
-                      includes="#include <gsl/gsl_rng.h>",
-                      Rcpp=TRUE,
-                      compileargs="-I/usr/include",
-                      libargs="-lgsl -lgslcblas")
-    cat("\n\nCalling second example without -DBeSilent set\n")
-    print(funx(0)["value"])
-
-    funx <- cfunction(signature(par="numeric"), gslrng,
-                      includes="#include <gsl/gsl_rng.h>",
-                      Rcpp=TRUE,
-                      compileargs="-I/usr/include -DBeSilent",
-                      libargs="-lgsl -lgslcblas")
-    cat("\n\nCalling second example with -DBeSilent set\n")
-    print(funx(1)["value"])
+    print(funx(0))
 
     invisible(NULL)
 }
