51ff6848b7ad45997e6209265f3345b840171d02
romain (R Enthusiast ) <francoisromain@free.fr>
2009-12-28 11:23:31 +0000

    improved exception handling
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@219 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/exceptions.R
+++ pkg/R/exceptions.R
@@ -15,11 +15,11 @@
 # You should have received a copy of the GNU General Public License
 # along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
 
-uncaught_cpp_exception <- function( message = "uncaught C++ exception" ){
+cpp_exception <- function( message = "C++ exception", class = NULL ){
 	callstack <- sys.calls()
 	ncalls <- length(callstack)
 	call <- if( ncalls > 1L) callstack[[ ncalls - 1L ]] else match.call()
-	classes <- c( "C++Error", "error", "condition" )
+	classes <- c( class, "C++Error", "error", "condition" )
 	condition <- structure( 
 		list( message = message, call = call ), 
 		class = classes )
