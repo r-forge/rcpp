d3396d61ecee30ed9541f850029c491fd5e78ff4
romain (R Enthusiast ) <francoisromain@free.fr>
2010-01-04 22:05:43 +0000

    new class Rcpp::Pairlist
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@274 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/inst/unitTests/runit.Pairlist.R
@@ -0,0 +1,54 @@
+#!/usr/bin/r -t
+#
+# Copyright (C) 2010	Dirk Eddelbuettel and Romain Francois
+#
+# This file is part of Rcpp.
+#
+# Rcpp is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Rcpp is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
+
+.setUp <- function(){
+	suppressMessages( require( inline ) )
+}
+
+test.Pairlist <- function(){
+	funx <- cfunction(signature(x="ANY"), 'return Pairlist(x) ;', 
+		Rcpp=TRUE, verbose=FALSE, includes = "using namespace Rcpp;" )
+	checkEquals( funx( pairlist("rnorm") ), pairlist("rnorm" ), msg = "Pairlist( LISTSXP )" )
+	checkEquals( funx( call("rnorm") ), pairlist("rnorm" ), msg = "Pairlist( LANGSXP )" )
+	checkEquals( funx(1:10), as.pairlist(1:10) , msg = "Pairlist( INTSXP) " )
+	checkEquals( funx(TRUE), as.pairlist( TRUE) , msg = "Pairlist( LGLSXP )" )
+	checkEquals( funx(1.3), as.pairlist(1.3), msg = "Pairlist( REALSXP) " )
+	checkEquals( funx(as.raw(1) ), as.pairlist(as.raw(1)), msg = "Pairlist( RAWSXP)" )
+	
+	checkException( funx(funx), msg = "Pairlist not compatible with function" )
+	checkException( funx(new.env()), msg = "Pairlist not compatible with environment" )
+	
+}
+
+test.Pairlist.variadic <- function(){
+	if( Rcpp:::canUseCXX0X() ){
+		funx <- cfunction(signature(), '
+		return Pairlist( "rnorm", 10, 0.0, 2.0 ) ;
+		', Rcpp=TRUE, verbose=FALSE, includes = "using namespace Rcpp;" )
+		checkEquals( funx(), pairlist("rnorm", 10L, 0.0, 2.0 ), 
+			msg = "variadic templates" )
+			
+		funx <- cfunction(signature(), '
+		return Pairlist( "rnorm", 10, Named("mean",0.0), 2.0 ) ;
+		', Rcpp=TRUE, verbose=FALSE, includes = "using namespace Rcpp;" )
+		checkEquals( funx(), pairlist("rnorm", 10L, mean = 0.0, 2.0 ), 
+			msg = "variadic templates (with names)" )
+	}
+}
+
---
+++ pkg/src/Pairlist.cpp
@@ -0,0 +1,64 @@
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; tab-width: 8 -*-
+//
+// Language.cpp: Rcpp R/C++ interface class library -- Language objects ( calls )
+//
+// Copyright (C) 2010	Dirk Eddelbuettel and Romain Francois
+//
+// This file is part of Rcpp.
+//
+// Rcpp is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Rcpp is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <Rcpp/Pairlist.h>
+#include <Rcpp/Evaluator.h>
+#include <Rcpp/RObject.h>
+#include <Rcpp/Language.h>
+#include <RcppCommon.h>
+
+namespace Rcpp {
+	
+	Pairlist::Pairlist( SEXP x = R_NilValue ) throw(not_compatible) : RObject::RObject( ){
+		
+		if( x != R_NilValue ){
+			if( TYPEOF( x ) == LISTSXP ){
+				// bingo 
+				setSEXP( x ) ;
+			} else if( TYPEOF( x) == LANGSXP ){
+				// almost
+				SET_TYPEOF(x, LISTSXP);
+				setSEXP( x) ;
+			} else {
+				Evaluator evaluator( Language("as.pairlist", x ) ) ;
+				evaluator.run() ;
+				if( evaluator.successfull() ){
+    					setSEXP( evaluator.getResult().asSexp() ) ;
+    				} else{
+    					throw not_compatible( ) ; 
+    				}
+			}
+		}
+		
+		
+	};
+	
+	Pairlist::~Pairlist(){}
+	
+	Pairlist::not_compatible::not_compatible() throw() {}
+    	const char* Pairlist::not_compatible::what() const throw(){
+    		return "cannot convert to pair list" ;
+    	}
+    	Pairlist::not_compatible::~not_compatible() throw() {}
+    
+	
+	
+} // namespace Rcpp
