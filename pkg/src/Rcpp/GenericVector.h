60d37d99c011ed8e07371184b96d8b82ed57a515
romain (R Enthusiast ) <francoisromain@free.fr>
2010-01-05 17:46:03 +0000

    + Rcpp::GenericVector (draft)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@284 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/src/Rcpp/GenericVector.h
@@ -0,0 +1,63 @@
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; tab-width: 8 -*-
+//
+// GenericVector.h: Rcpp R/C++ interface class library -- logical vectors
+//
+// Copyright (C) 2010	Dirk Eddelbuettel and Romain Francois
+//
+// This file is part of Rcpp.
+//
+// Rcpp is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Rcpp is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef Rcpp_GenericVector_h
+#define Rcpp_GenericVector_h
+
+#include <RcppCommon.h>
+#include <Rcpp/RObject.h>
+
+#ifdef HAS_INIT_LISTS
+#include <initializer_list>
+#include <algorithm>
+#endif
+
+namespace Rcpp{ 
+
+class GenericVector : public RObject {     
+public:
+
+	GenericVector(SEXP x) throw(not_compatible);
+	GenericVector( int size) ;
+	
+#ifdef HAS_INIT_LISTS	
+	GenericVector( std::initializer_list<RObject> list ) ;
+#endif
+	
+	/**
+	 * the length of the vector, uses Rf_length
+	 */
+	inline int length() const { return Rf_length( m_sexp ) ; }
+	
+	/**
+	 * alias of length
+	 */
+	inline int size() const { return Rf_length( m_sexp ) ; }
+	
+	SEXP get(const int& i) const ;
+	void set(const int& i, SEXP value ) ;
+} ;
+
+typedef GenericVector List ;
+
+} // namespace
+
+#endif
