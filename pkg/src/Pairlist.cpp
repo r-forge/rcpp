c5a0f89403a78e6c749571a28e4dc382959a35ff
romain (R Enthusiast ) <francoisromain@free.fr>
2010-01-10 20:42:38 +0000

    +Pairlist::operator[]
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@334 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/Pairlist.cpp
+++ pkg/src/Pairlist.cpp
@@ -62,7 +62,86 @@ namespace Rcpp {
 		}
 	}
 
-
+	Pairlist::Proxy::Proxy(Pairlist& v, const size_t& index) :
+		parent(v), index(index) {} ;
+	
+	Pairlist::Proxy& Pairlist::Proxy::operator=(const Proxy& rhs){
+		if( index < 0 || index >= parent.length() ) throw index_out_of_bounds() ;
+		if( rhs.index < 0 || rhs.index >=  rhs.parent.length() ) throw index_out_of_bounds() ;
+		
+		SEXP target = parent.asSexp() ;
+		SEXP origin = rhs.parent.asSexp();
+		size_t i=0; 
+		while( i < index ){
+			target = CDR(target) ;
+			i++; 
+		}
+		i=0; 
+		while( i < rhs.index ){
+			origin = CDR(origin) ;
+			i++;
+		}
+		SETCAR( target, CAR(origin) );
+		SET_TAG( target, TAG(origin) ); 
+		return *this ;
+	}
+	
+	Pairlist::Proxy& Pairlist::Proxy::operator=(const Named& rhs){
+		if( index < 0 || index >= parent.length() ) throw index_out_of_bounds() ;
+		size_t i = 0 ;
+		SEXP x = parent.asSexp() ; 
+		while( i < index ) {
+			x = CDR(x) ;
+			i++ ;
+		}
+		SETCAR( x, rhs.getSEXP() ) ;
+		SET_TAG( x, Symbol( rhs.getTag() ) ) ;
+		return *this ;
+	}
+	
+	Pairlist::Proxy& Pairlist::Proxy::operator=(SEXP rhs){
+		if( index < 0 || index >= parent.length() ) throw index_out_of_bounds() ;
+		SEXP x = parent.asSexp() ; 
+		size_t i = 0 ;
+		while( i < index ) {
+			x = CDR(x) ;
+			i++ ;
+		}
+		SETCAR( x, rhs) ;
+		return *this ;
+	}
+	
+	Pairlist::Proxy::operator SEXP() const{
+		if( index < 0 || index >= parent.length() ) throw index_out_of_bounds() ;
+		SEXP x = parent.asSexp() ; 
+		size_t i = 0 ;
+		while( i < index ) {
+			x = CDR(x) ;
+			i++ ;
+		}
+		return CAR(x) ;
+	}
+	
+	Pairlist::Proxy::operator RObject() const{
+		if( index < 0 || index >= parent.length() ) throw index_out_of_bounds() ;
+		SEXP x = parent.asSexp() ; 
+		size_t i = 0 ;
+		while( i < index ) {
+			x = CDR(x) ;
+			i++ ;
+		}
+		return wrap( CAR(x) ) ;
+	}
+	
+	const Pairlist::Proxy Pairlist::operator[](int i) const {
+		return Proxy( const_cast<Pairlist&>(*this), i) ;
+	}
+	
+	Pairlist::Proxy Pairlist::operator[](int i){
+		return Proxy( *this, i );
+	}
+	
+	
 	SEXP pairlist(){ return R_NilValue ; }
 
 } // namespace Rcpp
