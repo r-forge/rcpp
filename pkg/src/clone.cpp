3ecbcb431fbbef99d2a5d1c72bd4bd6c0ddc34e8
romain (R Enthusiast ) <francoisromain@free.fr>
2010-01-26 11:22:26 +0000

    correct include
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@464 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/clone.cpp
+++ pkg/src/clone.cpp
@@ -19,7 +19,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
 
-#include <clone.h>
+#include <Rcpp/clone.h>
 
 namespace Rcpp{
 
