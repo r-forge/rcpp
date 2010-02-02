75f3dc0bc4915ca230a28791c70695c510fce46f
romain (R Enthusiast ) <francoisromain@free.fr>
2010-02-02 07:45:56 +0000

    compiler error
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@555 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/Rcpp/traits/storage_type.h
+++ pkg/src/Rcpp/traits/storage_type.h
@@ -32,37 +32,50 @@ namespace traits{
  *
  * The default is SEXP, which works for VECSXP, EXPRSXP and STRSXP
  */
-template<int RTYPE> struct storage_type{ typedef SEXP type ; } ;
+template<int RTYPE> struct storage_type{ 
+	typedef SEXP type ;
+} ;
+
 
 /**
  * Total specialization for integer vector (INTSXP)
  * typedef to int
  */
-template<> struct storage_type<INTSXP>{  typedef int type ; } ;
+template<> struct storage_type<INTSXP>{ 
+	typedef int type ;
+} ;
 
 /**
  * Total specialization for numeric vectors (REALSXP)
  * typedef to double
  */
-template<> struct storage_type<REALSXP>{ typedef double type ; } ;
+template<> struct storage_type<REALSXP>{ 
+	typedef double type ;
+} ;
 
 /**
  * Total specialization for numeric vectors (CPLXSXP)
  * typedef to Rcomplex
  */
-template<> struct storage_type<CPLXSXP>{ typedef Rcomplex type ; } ;
+template<> struct storage_type<CPLXSXP>{ 
+	typedef Rcomplex type ;
+} ;
 
 /**
  * Total specialization for raw vectors (RAWSXP)
  * typedef to Rbyte
  */
-template<> struct storage_type<RAWSXP>{ typedef Rbyte type ; } ;
+template<> struct storage_type<RAWSXP>{ 
+	typedef Rbyte type ;
+} ;
 
 /**
  * Total specialization for logical vectors (LGLSXP)
  * typedef to int
  */
-template<> struct storage_type<LGLSXP>{ typedef int type ; } ;
+template<> struct storage_type<LGLSXP>{ 
+	typedef int type ;
+} ;
 
 } // traits
 } // Rcpp
