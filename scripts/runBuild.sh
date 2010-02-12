ea72f6552c21a83eea030caaf0a4374a62cd894c
edd (Dirk Eddelbuettel) <edd@debian.org>
2010-02-12 04:16:23 +0000

    a few minor updates
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@662 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- scripts/runBuild.sh
+++ scripts/runBuild.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+version=$(r -e'cat(as.character(read.dcf("pkg/DESCRIPTION")[,"Version"]))')
+echo "Working on version $version"
+
 export RCPP_CXX0X="yes"
 R CMD build --force pkg
+
+test -d tarballs    && cp -vax Rcpp_${version}.tar.gz tarballs/rcpp_${version}.orig.tar.gz
+test -d build-aread && cp -vax Rcpp_${version}.tar.gz build-area/rcpp_${version}.orig.tar.gz
+
