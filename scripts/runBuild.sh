b363d2d83c7098f7049f9c3f7e8aae5b54cfba00
edd <edd@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-02-13 03:38:54 +0000

    typo fix
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@666 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- scripts/runBuild.sh
+++ scripts/runBuild.sh
@@ -6,6 +6,6 @@ echo "Working on version $version"
 export RCPP_CXX0X="yes"
 R CMD build --force pkg
 
-test -d tarballs    && cp -vax Rcpp_${version}.tar.gz tarballs/rcpp_${version}.orig.tar.gz
-test -d build-aread && cp -vax Rcpp_${version}.tar.gz build-area/rcpp_${version}.orig.tar.gz
+test -d tarballs   && cp -vax Rcpp_${version}.tar.gz tarballs/rcpp_${version}.orig.tar.gz
+test -d build-area && cp -vax Rcpp_${version}.tar.gz build-area/rcpp_${version}.orig.tar.gz
 
