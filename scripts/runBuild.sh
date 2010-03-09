3cfcfa8209e4a8836645fbdd188e908de5544250
edd <edd@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-03-09 19:08:56 +0000

    a few improvements to helper scripts
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@864 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- scripts/runBuild.sh
+++ scripts/runBuild.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-version=$(r -e'cat(as.character(read.dcf("pkg/DESCRIPTION")[,"Version"]))')
+if [ ! -d Rcpp ]; then
+    echo "Not above Rcpp/"
+    exit -1
+fi
+
+version=$(r -e'cat(as.character(read.dcf("Rcpp/DESCRIPTION")[,"Version"]))')
 echo "Working on version $version"
 
 export RCPP_CXX0X="yes"
-R CMD build --force pkg
+R CMD build --force Rcpp
 
 test -d tarballs   && cp -vax Rcpp_${version}.tar.gz tarballs/rcpp_${version}.orig.tar.gz
 test -d build-area && cp -vax Rcpp_${version}.tar.gz build-area/rcpp_${version}.orig.tar.gz
