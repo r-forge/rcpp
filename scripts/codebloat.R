ee607fe89393f4a1393fe4168d52b34b4435262d
romain (R Enthusiast ) <francoisromain@free.fr>
2010-03-16 22:19:30 +0000

    forgot to commit (thanks Doug)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@910 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- scripts/codebloat.R
+++ scripts/codebloat.R
@@ -2,24 +2,24 @@
 #
 # to change the maximum number of arguments to 20
 # move to the root directory of Rcpp and do : 
-# Rscript inst/codebloat/codebloat.R 20
+# Rscript ../../scripts/codebloat.R 20
 #
 
 target <- commandArgs(TRUE)[1]
 
-typenames <- function( n = 1){
+typenames <- function( n = 1, txt){
 	paste( sprintf( "typename T%d", 1:n ), collapse = ", " )
 }
 
-arguments <- function( n = 1){
+arguments <- function( n = 1, txt){
 	paste( sprintf( "const T%d& t%d", 1:n, 1:n ), collapse = ", " )	
 }
 
-parameters <- function( n = 1 ){
+parameters <- function( n = 1, txt ){
 	paste( sprintf( "t%d", 1:n ), collapse = ", " )	
 }
 
-grow <- function( n = 1 ){
+grow <- function( n = 1, txt ){
 	grow__ <- function( n, m){
 		if( m > n ){
 			"R_NilValue"
@@ -30,16 +30,30 @@ grow <- function( n = 1 ){
 	grow__( n, 1 ) ;
 }
 
+for_each <- function(n = 1, txt ){
+	
+	rx <- "^.*[{][{](.*)[}][}].*$"
+	line <- grep( rx, txt )
+	expr <- sub( rx , "\\1", txt[line] )
+	res <- sapply( 1:n, function(index){
+		out <- gsub( "___I___", index - 1, expr )
+		out <- gsub( "___X___", sprintf( "t%d", index ), out )
+		out
+	} )
+	c( txt[ 1:(line-1) ], res, txt[ (line+1) : length(txt) ] )
+}
+
 modify <- function(txt, token, n, fun){
 	if( any( grepl( token, txt ) ) ){
-		gsub( token, fun(n), txt )
+		gsub( token, fun(n, txt), txt )
 	} else {
 		txt
 	}
-}
+}                        
 
 path <- file.path( "src", "Rcpp" )
 for( f in list.files(path, full.names = TRUE) ){
+#	f <- "src/Rcpp/make_list.h"
 	content <- readLines( f )
 	if( any( grepl( "code-bloat", content ) ) ){
 		first <- grep( "<code-bloat>", content )
@@ -53,6 +67,10 @@ for( f in list.files(path, full.names = TRUE) ){
 			txt <- modify( txt, "ARGUMENTS" , n, arguments  )
 			txt <- modify( txt, "PARAMETERS", n, parameters )
 			txt <- modify( txt, "GROW"      , n, grow )
+			txt <- modify( txt, "___N___"   , n, function(n,txt){ as.character(n) }  )
+			if( any( grepl( "__FOR_EACH__[{][{].*[}][}]", txt ) ) ){
+				txt <- for_each( n, txt )	
+			}
 			txt
 		} )
 		
@@ -63,7 +81,7 @@ for( f in list.files(path, full.names = TRUE) ){
 			output, 
 			content[last:length(content)] )
 		
-		cat( new.content, file = f, sep = "\n" )
+		cat( new.content, file = f , sep = "\n" )
 			
 	}
 }
