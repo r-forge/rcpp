667ab9bffffd61e987787271fffcacb5be5ba984
romain <romain@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-04-03 10:58:34 +0000

    package base64
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@953 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/base64/R/base64.R
@@ -0,0 +1,18 @@
+decode <- function(input, output = tempfile() ){
+	invisible( .Call( "decode_", input, output, PACKAGE = "base64" ) )
+}
+
+encode <- function(input, output = tempfile() , linesize = 72L ){
+	invisible( .Call( "encode_", input, output, as.integer(linesize), PACKAGE = "base64" ) )
+}
+
+img <- function( file, Rd = FALSE ){
+	tf <- tempfile()
+	on.exit( unlink(tf) )
+	encode( file, tf )
+	sprintf( '%s<img src="data:image/png;base64,\n%s" />%s', 
+		if( Rd ) "\\out{" else "", 
+		paste( readLines( tf) , collapse = "\n" ), 
+		if( Rd ) "}" else ""
+	)	
+}
