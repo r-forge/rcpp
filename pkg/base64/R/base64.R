b5f86eae8eaf8cb8738b8ce84b7d72096f0dd2f3
romain (R Enthusiast ) <francoisromain@free.fr>
2010-04-03 13:50:01 +0000

    added an alt argument to img
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@955 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/base64/R/base64.R
+++ pkg/base64/R/base64.R
@@ -6,13 +6,14 @@ encode <- function(input, output = tempfile() , linesize = 72L ){
 	invisible( .Call( "encode_", input, output, as.integer(linesize), PACKAGE = "base64" ) )
 }
 
-img <- function( file, Rd = FALSE ){
+img <- function( file, Rd = FALSE, alt = "image" ){
 	tf <- tempfile()
 	on.exit( unlink(tf) )
 	encode( file, tf )
-	sprintf( '%s<img src="data:image/png;base64,\n%s" />%s', 
+	sprintf( '%s<img src="data:image/png;base64,\n%s" alt="%s" />%s', 
 		if( Rd ) "\\out{" else "", 
 		paste( readLines( tf) , collapse = "\n" ), 
+		alt,
 		if( Rd ) "}" else ""
 	)	
 }
