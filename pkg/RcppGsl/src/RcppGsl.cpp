a7984f95b604d38769908801370b59c91063b754
romain (R Enthusiast ) <francoisromain@free.fr>
2010-04-04 14:02:32 +0000

    use implicit wrap through templated assignement op
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@982 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RcppGsl/src/RcppGsl.cpp
+++ pkg/RcppGsl/src/RcppGsl.cpp
@@ -4,7 +4,12 @@ extern "C" SEXP test_gsl_vector(){
 	gsl_vector * x = gsl_vector_alloc (10);
 	gsl_vector_set_zero( x ) ;
 	
-	Rcpp::NumericVector xx = Rcpp::wrap( *x ) ;
+	// we cannot have Rcpp::NumericVector xx = x ; 
+	// because this does not involve the assignment operator
+	// but the constructor and Vector only had templated assignement
+	// operator, not templated constructor, so we have to do this in two steps
+	Rcpp::NumericVector xx ;
+	xx = *x  ;
 	gsl_vector_free (x);
 	return xx ;
 }
