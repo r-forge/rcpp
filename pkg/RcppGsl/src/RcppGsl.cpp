b7e5821354c8d0bcfea0ad599ad50dba65fccb36
romain <romain@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-04-04 13:18:24 +0000

    add the typical unit test framework
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@979 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RcppGsl/src/RcppGsl.cpp
+++ pkg/RcppGsl/src/RcppGsl.cpp
@@ -2,6 +2,8 @@
 
 extern "C" SEXP test_gsl_vector(){
 	gsl_vector * x = gsl_vector_alloc (10);
+	gsl_vector_set_zero( x ) ;
+	
 	Rcpp::NumericVector xx = Rcpp::wrap( *x ) ;
 	gsl_vector_free (x);
 	return xx ;
