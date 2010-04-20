11b9b01d2819f9f2c54ade08e00ac2205a797bcc
romain <romain@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-04-20 16:30:21 +0000

    expose the return type of the RCPP_FUNCTION macro and add wrap into the mix so that both input and output get implicit conversion
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@1091 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- scripts/preprocessor.R
+++ scripts/preprocessor.R
@@ -4,20 +4,20 @@
 # Rscript ../scripts/preprocessor.R
 #
 
-res <- sapply( 0:65, function(i){
+rcpp_function <- sapply( 0:65, function(i){
 	txt <- sprintf( '
-#define RCPP_FUNCTION_%d(__NAME__%s)  \\
-SEXP RCPP_DECORATE(__NAME__)(%s) ;     \\
+#define RCPP_FUNCTION_%d(__OUT__,__NAME__%s)  \\
+__OUT__ RCPP_DECORATE(__NAME__)(%s) ;     \\
 extern "C" SEXP __NAME__(%s){          \\
 SEXP res = R_NilValue ;                 \\
 try{                                    \\
-	res = RCPP_DECORATE(__NAME__)(%s) ; \\
+	res = ::Rcpp::wrap( RCPP_DECORATE(__NAME__)(%s) ) ; \\
 } catch( std::exception& __ex__ ){      \\
 	forward_exception_to_r( __ex__ ) ;  \\
 }                                       \\
 return res ;                            \\
 }                                       \\
-SEXP RCPP_DECORATE(__NAME__)(%s)', 
+__OUT__ RCPP_DECORATE(__NAME__)(%s)', 
 	i,
 	if( i == 0 ) "" else paste( ",", paste( sprintf( "___%d", 0:(i-1)), collapse=", ") ),
 	if( i == 0 ) "" else paste( sprintf( "___%d", 0:(i-1)), collapse=", "),
@@ -26,9 +26,32 @@ SEXP RCPP_DECORATE(__NAME__)(%s)',
 	if( i == 0 ) "" else paste( sprintf( "___%d", 0:(i-1)), collapse=", ")
 	)
 })
+
+
+rcpp_function_nodecl <- sapply( 0:65, function(i){
+	txt <- sprintf( '
+#define RCPP_FUNCTION_NODECL_%d(__OUT__,__NAME__%s)  \\
+extern "C" SEXP __NAME__(%s){          \\
+SEXP res = R_NilValue ;                 \\
+try{                                    \\
+	res = ::Rcpp::wrap( RCPP_DECORATE(__NAME__)(%s) ) ; \\
+} catch( std::exception& __ex__ ){      \\
+	forward_exception_to_r( __ex__ ) ;  \\
+}                                       \\
+return res ;                            \\
+}                                       \\
+__OUT__ RCPP_DECORATE(__NAME__)(%s)', 
+	i,
+	if( i == 0 ) "" else paste( ",", paste( sprintf( "___%d", 0:(i-1)), collapse=", ") ),
+	if( i == 0 ) "" else paste( sprintf( "SEXP x%d", 0:(i-1) ), collapse = ", " ), 
+	if( i == 0 ) "" else paste( sprintf( "::Rcpp::internal::converter( x%d )", 0:(i-1) ), collapse = ", " ), 
+	if( i == 0 ) "" else paste( sprintf( "___%d", 0:(i-1)), collapse=", ")
+	)
+})
+
+
 res <- c( 
-"
-// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; tab-width: 8 -*-
+"// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; tab-width: 8 -*-
 //
 // preprocessor_generated.h: Rcpp R/C++ interface class library -- pre processor help
 //
@@ -51,7 +74,17 @@ res <- c(
 
 #ifndef RCPP__PREPROCESSOR_GENERATED_H
 #define RCPP__PREPROCESSOR_GENERATED_H
-", res, 
-"#endif" )
+
+// {{{ RCPP_FUNCTION 
+", rcpp_function, "
+// }}}
+
+// {{{ RCPP_FUNCTION_NODECL
+", rcpp_function_nodecl, 
+"
+// }}}
+#endif
+" )
+
 writeLines( res, "Rcpp/inst/include/Rcpp/preprocessor_generated.h" )
 
