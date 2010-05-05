8f3e6a4b5ed5097c97cd1167b37b5d38886f2710
edd <edd@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-05-05 01:10:08 +0000

    updated for inst/include/ location of header files
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@1166 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- scripts/runDoxygenAndZip.sh
+++ scripts/runDoxygenAndZip.sh
@@ -18,12 +18,12 @@ if [ -x /usr/bin/doxygen ]; then
     rm -rf html/ latex/ man/
     cd ${cwd}
     cd Rcpp
-    cd src && ln -s ../inst/examples . && cd -
+    cd src && ln -s ../inst/examples . && ln -s ../inst/include . && cd -
 
     ## see FAQ 17 for doxygen
     ( cat doxyfile ; echo PROJECT_NAME="\"Rcpp Version ${version}\"" ) | doxygen -
 
-    rm src/examples
+    rm src/examples src/include
     cd ${cwd}
     pwd
 
