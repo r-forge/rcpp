e957f76147d8662217b4342c0f2d2bdb45cee93d
edd (Dirk Eddelbuettel) <edd@debian.org>
2010-11-12 03:28:41 +0000

    splitting RcppParDE off RcppDE so that we can compare the approach with OpenMP (in RcppParDE) to the plain C++ approach (in RcppDE)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@2430 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/RcppParDE/R/zzz.R
@@ -0,0 +1,9 @@
+.onLoad <- function (lib, pack) {
+    packageStartupMessage("RcppParDE package")
+    packageStartupMessage("  C++ / OpenMP Implementation of Differential Evolution Optimisation")
+    packageStartupMessage("  Author: Dirk Eddelbuettel")
+    packageStartupMessage("Based on:")
+    packageStartupMessage("  DEoptim package")
+    packageStartupMessage("  Differential Evolution algorithm in R")
+    packageStartupMessage("  Authors: David Ardia and Katharine Mullen")
+}
--- pkg/RcppParDE/scripts/openmp.r
+++ pkg/RcppParDE/scripts/openmp.r
@@ -4,12 +4,22 @@
 
 suppressMessages(library(DEoptim)) 	# the original, currently 2.0.7
 suppressMessages(library(RcppDE))    	# the contender
+suppressMessages(library(RcppParDE))    # the parallel approach
 suppressMessages(library(inline))    	# helps with
 
 Genrose <- function(x) { 		## One generalization of the Rosenbrock banana valley function (n parameters)
     n <- length(x)
     1.0 + sum (100 * (x[-n]^2 - x[-1])^2 + (x[-1] - 1)^2)
 }
+
+Wild <- function(x) { 		## 'Wild' function, global minimum at about -15.81515
+    sum(10 * sin(0.3 * x) * sin(1.3 * x^2) + 0.00001 * x^4 + 0.2 * x + 80)/length(x)
+}
+
+Rastrigin <- function(x) {
+    sum(x+2 - 10 * cos(2*pi*x)) + 20
+}
+
 
 
 #maxIt <- 500                           # not excessive but so that we get some run-time on simple problems
@@ -63,11 +73,18 @@ src.xptr <- 'std::string fstr = Rcpp::as<std::string>(funname);
                  '
 
 create_xptr <- cxxfunction(signature(funname="character"), body=src.xptr, inc=inc, plugin="Rcpp")
-xptr <- create_xptr("genrose")
+xptr <- create_xptr("wild")
+func <- Wild
 
 set.seed(42)
 print(system.time( {
     res <- RcppDE::DEoptim(fn=xptr, lower=rep(-25, n), upper=rep(25, n), control=list(NP=10*n, itermax=maxIt, trace=FALSE))
+    print(res[[1]][["bestval"]])
+}))
+
+set.seed(42)
+print(system.time( {
+    res <- RcppParDE::DEoptim(fn=xptr, lower=rep(-25, n), upper=rep(25, n), control=list(NP=10*n, itermax=maxIt, trace=FALSE))
     print(res[[1]][["bestval"]])
 }))
 
