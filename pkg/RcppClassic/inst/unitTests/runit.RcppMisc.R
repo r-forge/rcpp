35c9cf495039d6b7cb81d82faf9e5222be623450
romain <romain@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-12-02 20:14:33 +0000

    migrate unit tests about classic api into RcppClassic
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@2678 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RcppClassic/inst/unitTests/runit.RcppMisc.R
+++ pkg/RcppClassic/inst/unitTests/runit.RcppMisc.R
@@ -3,22 +3,20 @@
 #
 # Copyright (C) 2010	Dirk Eddelbuettel and Romain Francois
 #
-# This file is part of Rcpp.
+# This file is part of RcppClassic.
 #
-# Rcpp is free software: you can redistribute it and/or modify it
+# RcppClassic is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 2 of the License, or
 # (at your option) any later version.
 #
-# Rcpp is distributed in the hope that it will be useful, but
+# RcppClassic is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
-
-if( Rcpp:::capabilities()["classic api"] ){
+# along with RcppClassic.  If not, see <http://www.gnu.org/licenses/>.
 
 .setUp <- function() {
 
@@ -97,7 +95,7 @@ if( Rcpp:::capabilities()["classic api"] ){
 
         signatures <- lapply(f, "[[", 1L)
         bodies <- lapply(f, "[[", 2L)
-        fun <- cxxfunction( signatures, bodies, plugin = "Rcpp")
+        fun <- cxxfunction( signatures, bodies, plugin = "RcppClassic")
         getDynLib( fun ) # just forcing loading the dll now
         assign( tests, fun, globalenv() )
     }
@@ -145,5 +143,4 @@ test.RcppParams.Datetime <- function() {
     attr(posixt, "tzone") <- NULL    ## because we don't set a tzone attribute in C++
     result <- fun(list(val=posixt))[[1]]
     checkTrue( (result-posixt) == 0.0 , msg = "RcppParams.getDatetimeValue")
-}
 }
