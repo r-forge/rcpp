580df179c1373067deff631d2e8ec0f6dca28122
edd (Dirk Eddelbuettel) <edd@debian.org>
2012-10-31 13:08:38 +0000

    minor tweak to allow full build and test without copies to the web and build directories
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcpp@3868 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- scripts/runBuild.sh
+++ scripts/runBuild.sh
@@ -7,6 +7,37 @@
 # Copies tarball to web directory too
 
 set -e 
+set -u 
+
+progname=`basename $0`
+options='nh?'
+ 
+usage_and_exit()
+{
+    echo ""
+    echo "Usage: $progname [-n] [-?|-h]"
+    echo "  Run build and tests for Rcpp package"
+    echo ""
+    echo "Options:"
+    echo "  -n       no copy, only local build tests"
+    echo "  -h       show this help"
+    echo ""
+    exit 0
+}
+
+copy=1
+while getopts "$options" i 
+do 
+    case "$i" in
+	n)
+	    copy=0
+	    shift
+	    ;;
+	h|?)
+	    usage_and_exit
+	    ;;
+    esac
+done
 
 if [ ! -d Rcpp ]; then
     echo "Not above Rcpp/"
@@ -20,14 +51,19 @@ echo "Working on:  $encodedversion -- $version"
 #export RCPP_CXX0X="yes"
 export RCPP_CXX0X="no"
 
+export RunAllRcppTests="yes"
+
 ## remove old pdf vignettes and make fresh ones
-cd Rcpp/inst/doc && make pdfclean && make && cd - 
+cd Rcpp/inst/doc && make pdfclean && make pdfall && cd - 
 
 R CMD build --force Rcpp
 
-R CMD check Rcpp_${version}.tar.gz
+R CMD check --as-cran Rcpp_${version}.tar.gz
 
-test -d tarballs   && cp -vax Rcpp_${version}.tar.gz tarballs/rcpp_${version}.orig.tar.gz
-test -d build-area && cp -vax Rcpp_${version}.tar.gz build-area/rcpp_${version}.orig.tar.gz
-test -d ~/www/code/rcpp && cp -vax Rcpp_${version}.tar.gz ~/www/code/rcpp
+if [ ${copy} -eq 1 ]; then
+    test -d tarballs   && cp -vax Rcpp_${version}.tar.gz tarballs/rcpp_${version}.orig.tar.gz
+    test -d build-area && cp -vax Rcpp_${version}.tar.gz build-area/rcpp_${version}.orig.tar.gz
+    test -d ~/www/code/rcpp && cp -vax Rcpp_${version}.tar.gz ~/www/code/rcpp
+fi
 
+echo "Done with $encodedversion -- $version"
